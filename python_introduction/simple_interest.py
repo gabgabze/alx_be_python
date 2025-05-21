834755b7193e57c631570567408ff9625e87c41d
waregagbagbo <gabelwarega@gmail.com>
2025-05-22 02:03:23 +0300

    Modified the variables correctly

--- python_introduction/simple_interest.py
+++ python_introduction/simple_interest.py
@@ -1,7 +1,7 @@
 #initialize the interest varibles
-p = 1000
-r = 5/100
-t = 3
-interest = p * r * t
+principle = 1000
+rate = 0.05
+time = 3
+interest = principle * rate * time
 
-print("The simple interest is:", interest)
+print("The simple interest is:",interest)
