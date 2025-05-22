8a90fad5314189a5e84908c52fe6206337843792
waregagbagbo <gabelwarega@gmail.com>
2025-05-22 03:09:29 +0300

    Calculated the user future age logic

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,10 @@
+current_year = 2023
+future_year = 2050
+
+gap_year = future_year -  current_year
+
+userAge = int(input('what is your age? '))
+
+futureAge = userAge + gap_year
+
+print("In", future_year, "you will be", futureAge, 'years old.')
