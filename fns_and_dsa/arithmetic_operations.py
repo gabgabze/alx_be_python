891549fbd941859a82ba1966d4ca53c9bd52a1fc
gabgabze <denzrich10@gmail.com>
2025-06-08 11:36:32 +0300

    Added the arithmetic logic

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,29 +1,13 @@
 def perform_operation(num1,num2,operation):
-    number1 = float(num1)
-    number2 = float(num2)
-    operation = str(operation)
-    match operation:
-        case 'add':
-            return number1+number2
-        case 'subtract':
-            return number1-number2
-        case 'multiply':
-            return number1*number2
-        case 'divide':
-            if number1 > 0  and number2 == 0:
-                return 'Cannot divide by zero'
-            return number1/number2
+    num1 = float(num1)
+    num2 = float(num2)
 
-"""    if operation == "+":
-        return number1 + number2
-    if operation == "-":
-        return number1 - number2
-    if operation == "*":
-        return number1 * number2
-    if operation == "/":
-        if number1 > 0 and number2 == 0:
-            return'Cannot divide by zero'
-        
-            return number1 / number2"""
-
-print(perform_operations(1,3,"+"))
+    if  operation == "add":
+        return num1 + num2
+    if operation == "subtract":
+        return num1 - num2
+    if operation == "multiply":
+        return num1 * num2
+    if operation == "divide" and num2 == 0:
+        return 'Cannot divide by zero'
+    return num1 / num2
