2770636e1b8237af4dd920b3250e6f76436d5346
gabgabze <denzrich10@gmail.com>
2025-06-08 12:10:46 +0300

    Added the arithmetic logic

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -9,5 +9,5 @@ def perform_operation(num1, num2, operation):
         case "divide":
             if num1 > 0 and num2 >0:
                 return num1 / num2
-            elif num1 > 0 and num2 ==0:
+            elif num1 > 0 and num2 == 0:
                 return 'Cannot divide by zero'
