8d10cd3dd8e5297dadd9bfe6fd36a7b4af66eeaa
gabgabze <denzrich10@gmail.com>
2025-06-14 18:32:16 +0300

    added the OOP logics to the operation

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,18 @@
+class BankAccount:
+
+    def __init__(self, account_number,initial_balance=0):
+        self.account_number = account_number
+        self.balance = initial_balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        if self.balance < 0:
+            return False
+        else:
+            return True
+
+    def display_balance(self):
+        return self.balance
