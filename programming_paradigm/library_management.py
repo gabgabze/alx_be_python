7ed3b0860517e4bcc565b0bd94bea30323bf7bcd
gabgabze <denzrich10@gmail.com>
2025-06-15 16:41:42 +0300

    added the library class

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,14 +1,14 @@
 class Book:
     """create instance with private attribute"""
-    def __init__(self,title,author, is_checked_out):
-        self.is_checked_out = is_checked_out
-        self.title = title
-        self.author = author
+    def __init__(self,title,author,_is_checked_out):
+        self.title=title
+        self.author=author
+        self.is_checked_out=_is_checked_out
 
 """implement library class"""
 class Library(Book):
     def __init__(self,books):
-        self.books = books
+        self.books = []
 
     """add methods for this operation"""
     def add_book(self, book):
