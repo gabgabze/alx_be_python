f61061889683a6c6939f390754824d4d3ee0787c
gabgabze <denzrich10@gmail.com>
2025-06-22 14:33:54 +0300

    added the files for the inheritance

--- oop/library_system.py
+++ oop/library_system.py
@@ -4,21 +4,25 @@ class Book:
         self.title = title
         self.author = author
 
-# create sub_classes Ebook and printBook that inherit from Book
+# create sub_classes Ebook and PrintBook that inherit from Book
 class EBook(Book):
-    def __init__(self, title, author,file_size):
+    def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size
 
-
 class PrintBook(Book):
-    def __init__(self, title, author, page_count = 0):
+    def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
-        
-# create a Library class that has all the other classes
+
+# create a Library class that holds books
 class Library:
-    pass
-"""create methods in library class
-   Adds a Book, EBook, or PrintBook instance to the library.
-   list_books(self): Prints details of each book in the library."""
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+        return book
+
+    def list_books(self):
+        return self.books
